4c247586baeef8a16f590a61c351e465d67915b9
doai8912 <112995618+doai8912@users.noreply.github.com>
2023-04-10 11:25:37 +0900

    Update kakaoMap.js

--- kakaoMap.js
+++ kakaoMap.js
@@ -6,23 +6,6 @@ var options = {
 };
 
 var map = new kakao.maps.Map(container, options);
-
-// "지도 중심좌표 이동시키기"
-function setCenter() {
-    // 이동할 위도 경도 위치를 생성한다.
-    var moveLatLon = new kakao.maps.LatLng(33.452613, 126.570888);
-
-    // 지도 중심을 이동시칸다.
-    map.setCenter(moveLatLon);
-}
-
-// "지도 중심좌표 부드럽게 이동시키기"
-function panTo() {
-    // 이동할 위도 경도 위치를 생서한다
-    var moveLatLon = new kakao.maps.LatLng(33.450580, 126.574942);
-
-    map.panTo(moveLatLon)
-}
 };
 
 $(function() {
